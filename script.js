bad2a50c152bda2cd810adf449e9e9dbd2903582
mikorayuki <mikorayuki@gmail.com>
2026-08-04 01:42:47 +0000

    update

--- script.js
+++ script.js
@@ -4,11 +4,9 @@ function updateIcon() {
   toggleButton.textContent = document.body.classList.contains('dark') ? '☀️' : '🌙';
 }
 
-// Toggle dark mode
 toggleButton.addEventListener('click', () => {
   document.body.classList.toggle('dark');
   updateIcon();
 });
 
-// Set ikon awal
 updateIcon();
